3bf89253bc175695ff07b27af09baad480a0eadf
Justin Sitter <justin@jaytria.com>
2017-10-15 19:38:23 -0700

    add timestamp

--- routes/router.js
+++ routes/router.js
@@ -64,22 +64,20 @@ module.exports = (app)=>{
         res.render('offer-document');
     })
 
-    app.get('vaultgate', (req, res)=>{
+    app.post('/vaultgate', (req, res)=>{
 
         if (!req.files)
         return res.status(400).send('No files were uploaded.');
      
-      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-      let sampleFile = req.files.sampleFile;
-     
-      // Use the mv() method to place the file somewhere on your server
-      sampleFile.mv('/imgs/filename.jpg', function(err) {
-        if (err)
-          return res.status(500).send(err);
+        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        //let sampleFile = req.files.userDocument;
+        console.log(+new Date)
+        // Use the mv() method to place the file somewhere on your server
+       // sampleFile.mv('/imgs/filename.jpg', function(err) {
+        //if (err)
+        //  return res.status(500).send(err);
      
         res.send('File uploaded!');
       });
-    })
-
 }
 
