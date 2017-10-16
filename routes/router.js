77103e681b444686f80f248a0eed07cd3d56b002
Justin Sitter <justin@jaytria.com>
2017-10-15 19:31:16 -0700

    add file upload logic

--- routes/router.js
+++ routes/router.js
@@ -64,5 +64,22 @@ module.exports = (app)=>{
         res.render('offer-document');
     })
 
+    app.get('vaultgate', (req, res)=>{
+
+        if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+     
+      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+      let sampleFile = req.files.sampleFile;
+     
+      // Use the mv() method to place the file somewhere on your server
+      sampleFile.mv('/imgs/filename.jpg', function(err) {
+        if (err)
+          return res.status(500).send(err);
+     
+        res.send('File uploaded!');
+      });
+    })
+
 }
 
