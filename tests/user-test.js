a9e38b1477b27ba11ed9bf6fa83be5cdd1db4526
Justin Sitter <justin@jaytria.com>
2018-06-11 14:30:07 -0700

    added done to end of test

--- tests/user-test.js
+++ tests/user-test.js
@@ -1,16 +1,16 @@
 const assert = require('chai').assert;
 
 var request = require('supertest');
-const express = require('express')
+const express = require('express');
 
 request = request('http://localhost:5000')
 describe('Index', function() {
-    const app = require('../index')
+    const app = require('../index');
     it('should return html', function(done) {
       
       request.get('/')
       .expect('Content-Type', 'text/html; charset=utf-8', done);
-
+      done();
     });
-
+    
 });
