5648b478fc65ba9ccc2238ec761392613002f1f4
Justin Sitter <justin@jaytria.com>
2019-03-11 14:42:09 -0700

    add password reset form routes

--- routes/pw_resets.js
+++ routes/pw_resets.js
@@ -4,8 +4,8 @@ module.exports = (app) => {
   const jwt = require('jsonwebtoken');
   const nodemailer = require('nodemailer');
   const mg = require('nodemailer-mailgun-transport');
-  const site_address = process.env.SITE || "localhost:"+ port
-
+  const site_address = process.env.SITE || "localhost:"+ port;
+  const bcrypt = require('bcrypt');
   const auth = {
     auth: {
       api_key: process.env.MAILGUN_API_KEY,
@@ -84,17 +84,69 @@ app.get('/reset/:token/:email', (req, res)=>{
     if (req.params.email){
       User.findOne({email:req.params.email, pw_reset_token: req.params.token}).then((user)=>{
         console.log(user)
+        if( user.pw_reset_token == req.params.token){
+          console.log("Login user")
+          
+          //logging in user
+          var token = jwt.sign({ _id: user._id }, process.env.SECRETKEY, { expiresIn: "60 days" });
+          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+          res.redirect('/reset-form')
+        }
       })
+    }else{
+      res.render('expired');
     }
+  }else{
+    res.render('expired');
   }
 
-  res.render('expired');
+  
+})
+
+app.get('/reset-form', (req, res)=>{
+  if(!req.user){
+    res.redirect('/')
+  }else{
+    res.render('password-form')
+  }
+  
 })
 
 /************************
 *     Authenticated Password Reset (POST)
 ************************/
-app.post('/reset', (req, res)=>{
-  res.render('expired');
+app.post('/reset-done', (req, res)=>{
+  
+  let hashed_pass = '';
+
+  console.log("reset done ", req.user);
+  
+  if(req.user){
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+        User.updateOne(
+          {_id:req.user}, 
+          { $set: {
+            password: hash,
+            pw_reset_request: "",
+            pw_reset_token: ""
+          }}
+          ).then((user)=>{
+            console.log("User data", user)
+            var token = jwt.sign({ _id: user._id }, process.env.SECRETKEY, { expiresIn: "60 days" });
+              res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+
+              res.redirect('/u');
+          }).catch( (err)=>console.log(err));
+          
+
+      });
+    });
+
+      
+  }else{
+    res.render('expired');
+  }
+  
 })
 };
