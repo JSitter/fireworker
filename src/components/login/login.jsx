ebe7db6ded3727906d579aebafe9645eb022ef2a
Justin Sitter <justin@jaytria.com>
2019-12-26 16:14:49 -0800

    Add React functionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.scss';
 
 function Login(props){
+  const [ email, setEmail ] = useState(null);
+  const [ password, setPassword ] = useState(null);
+  const [ passDisplaySetting, setPassDisplaySetting] = useState('password');
+  function handleEmailChange(event){
+    setEmail(event.target.value);
+  }
+
+  function handlePassChange(event){
+    setPassword(event.target.value);
+  }
+
+  function handleViewPassword(){
+    if(passDisplaySetting == "text"){
+      setPassDisplaySetting("password");
+    }else{
+      setPassDisplaySetting("text");
+    }
+  };
+
   return (
     <div className="form-wrapper"> 
     <div className="close-x" onClick={()=>props.setFormState('none')}><i class="fas fa-times"></i></div>
       
       <form action="">
-              <label htmlFor="email" className="required-field">Email</label>
-              <input type="text" name="email" id="email"></input>
+        <label htmlFor="email" className="required-field">Email</label>
+        <input type="text" name="email" id="email" onChange={handleEmailChange} value={email} ></input>
+        
+        <label htmlFor="password" className="required-field">Password</label>
+        <input type={passDisplaySetting} name="password" id="password" onChange={handlePassChange} value={password}></input>
+        <span className="password-hint" onClick={()=>handleViewPassword()} >
+          {passDisplaySetting === "text" ? 'Hide Password' : 'View Password'}
+        </span>
 
-              <label htmlFor="password1" className="required-field">Password</label>
-              <input type="text" name="password1" id="password1"></input>
-
-              <label htmlFor="password2" className="required-field">Re Enter Password</label>
-              <input type="text" name="password2" id="password2"></input>
-
-              <section className="check-section">
-                  <input type="checkbox" id="chk1-label"></input>
-                  <label for="consent">I agree to allow cookies.</label>
-              </section>
-              <input className="submit" type='submit' />
+        <section className="check-section">
+            <label for="consent">
+            <input type="checkbox" id="chk1-label"></input>
+            I agree to allow cookies.</label>
+        </section>
+        <input className="submit" type='submit' />
       </form>
       <p><span className="spalink" onClick={()=>props.setFormState("register")}> Create New Account...</span></p>
     </div>
--- utils/lib.js
+++ utils/lib.js
@@ -1,3 +1,41 @@
-module.exports = () => {
+// Front end library with access to fetch
+let fetchData = function(url, payload, method){
+  let headers;
 
+  if(method != "POST"){
+    method = "GET"
+    headers = 'application/json';
+
+  }else{
+    headers = 'application/x-www-form-urlencoded';
+  }
+
+  let reqParams = {
+    method : method,
+    mode : "cors",
+    credentials: "include",
+    headers: {
+      'Content-Type': headers,
+    },
+    body: JSON.stringify(payload)
+  }
+
+  fetch(url, reqParams).then((response)=>{
+    if(response.status != 200){
+      throw new Error(response.text);
+    }
+    response.json().then((data)=>{
+      return data;
+    });
+  })
+  .catch((err)=>new Error(String(err)));
+};
+
+export function userLogin(userName, password1){
+  console.log("logging in");
+  return {_uid: "myuseride"}
+}
+
+export function userRegister(userData){
+  return {_uid: "testuserid"}
 }
