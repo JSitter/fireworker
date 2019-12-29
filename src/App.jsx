16035e16441e5a52eb22fb2751d8c7ddee538570
Justin Sitter <justin@jaytria.com>
2019-12-29 00:26:39 -0800

    Add cookie verification

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import './App.scss';
 import Splash from './components/splash/splash.jsx';
 import Dashboard from './components/dashboard/dashboard.jsx';
+import { getCookie } from './utils/lib';
 
 function App() {
   const [ user, setUser ] = useState(null);
+  const [ serverCookie, setServerCookie ] = useState(null);
+
+  useEffect(()=>{
+    console.log("First Run")
+    getCookie().then((cookie) => {
+      setServerCookie(cookie.fToken);
+    }).catch((err)=>{
+      //User isn't Logged in
+    })
+  }, [setServerCookie]);
+
   return (
     <div>
-      { user ? <Dashboard user={user}/> : <Splash setUser={setUser}/> }
+      { (serverCookie || user ) ? <Dashboard user={user}/> : <Splash setUser={setUser}/> }
     </div>
   )
 }
