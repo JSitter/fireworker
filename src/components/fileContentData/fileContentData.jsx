a2f9dde63895858e1d293013ca2e80752189b1c1
Justin Sitter <justin@jaytria.com>
2020-01-05 13:38:06 -0800

    Add positioning and classnames

--- src/components/fileContentData/fileContentData.jsx
+++ src/components/fileContentData/fileContentData.jsx
@@ -2,14 +2,42 @@ import React, { useState, useEffect } from 'react';
 import './fileContentData.scss';
 
 function FileRow(props){
-    return (
-    <tr>
-        <td>{props.rowData.name}</td>
-        <td>{props.rowData.size / 1000} Kbytes</td>
-        <td>{props.rowData.type || 'n/a'}</td>
-        <td><i class="fas fa-trash" onClick={()=>props.deleteFile(props.fileIndex)}></i></td>
-        </tr>
-    )
+    const [ infoVisibility, setInfoVisibility ] = useState(false);
+
+    function handleInfo(){
+        infoVisibility ? setInfoVisibility(false) : setInfoVisibility(true);
+    }
+
+    function BasicRow(){
+        return (
+            <tr onClick={handleInfo} className="basic-file-data">
+                <td>{props.rowData.name}</td>
+                <td>{props.rowData.size / 1000} Kbytes</td>
+                <td>{props.rowData.type || 'n/a'}</td>
+                <td><i class="fas fa-trash" onClick={()=>props.deleteFile(props.fileIndex)}></i></td>
+            </tr>
+        )
+        
+    }
+
+    function DetailRow(){
+        return (
+            <tr onClick={handleInfo} className="detailed-file-data">
+                <td colspan="4">{props.rowData.name}<br/>
+                {props.rowData.size / 1000} Kbytes<br/>
+                {props.rowData.type || 'n/a'}<br/>
+                <i class="fas fa-trash" onClick={()=>props.deleteFile(props.fileIndex)}></i></td>
+            </tr>
+        )
+    }
+
+    if(infoVisibility){
+        return DetailRow()
+    }else{
+        return BasicRow();
+    }
+    
+
 }
 
 function FileContentData(props){
@@ -34,13 +62,13 @@ function FileContentData(props){
 
     return (
         <div className="file-data-wrapper">
-            <table>
+            <table className="file-data-table">
                 <tr>
                   <th>File Name</th><th>Size</th><th>File Type</th><th><i class="fas fa-minus-circle" onClick={props.deleteAllFiles}></i></th>
                 </tr>
                 {uploadedFiles}
             </table>
-            <span className="upload-button">Upload</span>
+            <span className="upload-button">Upload Files</span>
         </div>
     );
 }
