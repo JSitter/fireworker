148236760860b87562fb22bd613cc361f216b9a3
Justin Sitter <justin@jaytria.com>
2021-05-12 14:46:12 -0700

    Use fontawesome

--- src/components/topMenu/topMenu.jsx
+++ src/components/topMenu/topMenu.jsx
@@ -5,10 +5,10 @@ function TopMenu(props){
     return (
        <section className="top-menu">
            <h1>F</h1>
-           <i class="fas fa-users"></i>
-           <i class="fas fa-user-plus"></i>
-           <i class="fas fa-file-archive"></i>
-           <i class="fas fa-user"></i>
+           <i className="fas fa-users"></i>
+           <i className="fas fa-user-plus"></i>
+           <i className="fas fa-file-archive"></i>
+           <i className="fas fa-user"></i>
        </section>
     );
 }
